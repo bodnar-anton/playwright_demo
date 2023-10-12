18358df143aea63c6e07e7879ed1b76911a7a575
Anton Bodnar <toni.bodnar.2014@gmail.com>
2023-10-12 21:01:29 +0200

    Remove negative test cases

--- src/test/specs/contact_us_form.spec.ts
+++ src/test/specs/contact_us_form.spec.ts
@@ -12,65 +12,6 @@ const validEmail = 'AndriiPivtorak@luxequality.com';
 const validMessage = 'It is interesting to learn about your services, get advice on my project and discuss the free trial in more detail.\nThank you!\nWaiting for feedback!';
 
 test.describe('Contact Us form test', () => {
-    test.skip('Submit a form with an empty input fields', async ({ page }) => {
-        const contactUsPage = new ContactUsPage(page);
-
-        // Navigate to the «Contact Us» page
-        await contactUsPage.navigate();
-        await contactUsPage.isPageOpened();
- 
-        // Verify the «Contact With Us» form is displayed
-        await contactUsPage.isContactWithUsFormDisplayed();
-        await contactUsPage.isFullNameInputFieldDisplayed();
-        await contactUsPage.isEmailInputFieldDisplayed();
-        await contactUsPage.isMessageInputFieldDisplayed();
-        await contactUsPage.isPrivacyPolicyCheckboxDisplayed();
-        await contactUsPage.isSendMessageButtonDisplayed();
-
-        // Click the «Send Message» button
-        await contactUsPage.clickSendMessageButton();
-        await contactUsPage.isFullNameInputFieldErrorDisplayed(ContactUsPage.REQUIRED_FIELD_ERROR);
-        await contactUsPage.isEmailInputFieldErrorDisplayed(ContactUsPage.REQUIRED_FIELD_ERROR);
-        await contactUsPage.isMessageInputFieldErrorDisplayed(ContactUsPage.REQUIRED_FIELD_ERROR);
-        await contactUsPage.isPrivacyPolicyCheckboxErrorDisplayed(ContactUsPage.ACCEPT_PRIVACY_POLICY_ERROR);
-    });
-
-    test.skip('Submit a form with an invalid email', async ({ page }) => {
-        const contactUsPage = new ContactUsPage(page);
-
-        // Navigate to the «Contact Us» page
-        await contactUsPage.navigate();
-        await contactUsPage.isPageOpened();
-
-        // Verify the «Contact With Us» form is displayed
-        await contactUsPage.isContactWithUsFormDisplayed();
-        await contactUsPage.isFullNameInputFieldDisplayed();
-        await contactUsPage.isEmailInputFieldDisplayed();
-        await contactUsPage.isMessageInputFieldDisplayed();
-        await contactUsPage.isPrivacyPolicyCheckboxDisplayed();
-        await contactUsPage.isSendMessageButtonDisplayed();
-
-        // Enter the full name
-        await contactUsPage.enterFullName(randomFullName);
-        await contactUsPage.isFullNameEntered(randomFullName);
-
-        // Enter the invalid email
-        await contactUsPage.enterEmail(invalidEmail);
-        await contactUsPage.isEmailEntered(invalidEmail);
-
-        // Enter the message
-        await contactUsPage.enterMessage(randomMessage);
-        await contactUsPage.isMessageEntered(randomMessage);
-
-        // Check the «Privacy Policy» checkbox
-        await contactUsPage.clickPrivacyPolicyCheckbox();
-        await contactUsPage.isPrivacyPolicyCheckboxChecked();
-
-        // Click the «Send Message» button
-        await contactUsPage.clickSendMessageButton();
-        await contactUsPage.isEmailInputFieldErrorDisplayed(ContactUsPage.INVALID_EMAIL_ERROR);
-    });
-
     test('Submit a form with the valid data', async ({ page }) => {
         const homePage = new HomePage(page);
         const contactUsPage = new ContactUsPage(page);
